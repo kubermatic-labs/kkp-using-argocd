ef60c0695bf43ddde6f72132a7541ccfbcf978f9
Vijay Dharap <dharapvj@gmail.com>
2025-02-10 05:00:04 +0000

    updated the bucket location

--- kubeone-install/dev-seed/provider.tf
+++ kubeone-install/dev-seed/provider.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "kubermatic-e2e-test-tf"
+    region = "eu-central-1"
     # bucket = "cluster-backup-e2e"
+    # region = "eu-west-1"
     key    = "kkp-argocd-test/tfstate/dev-seed"
-    region = "eu-west-1"
   }
 }
